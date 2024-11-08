a1de861a8f9a07e066cce72d56490332e966cab0
John Doe <johndoe@example.com>
2024-11-08 13:46:30 +0100

    soutpole map update

--- src/content/southpole/SouthPoleView.tsx
+++ src/content/southpole/SouthPoleView.tsx
@@ -1,38 +1,39 @@
-import { ReactNode, useEffect, useRef, useState } from "react";
-import { useWindowResize } from "../../hooks/useWindowResize";
-import Page from "../../components/Page";
-import { CanvasWrapper } from "../../components/CanvasWrapper";
-import { Canvas } from "../../components/Canvas";
-import { CanvasZoomControl } from "../../components/CanvasZoomControl";
-import { CanvasLayerControl } from "../../components/CanvasLayerControl";
-import { Navigator } from "../../components/Navigator";
-import { Sidepanel2, Sidepanel2Controller } from "../../components/Sidepanel2";
-import { OverlayProvider, useOverlay } from "../../components/Overlay";
-import { ChevronDownIcon, ChevronUpIcon, InformationCircleIcon } from "@heroicons/react/24/solid";
-import Icon from "../../components/Icon";
-import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
+import { ReactNode, useEffect, useRef, useState } from "react"
+import { useWindowResize } from "../../hooks/useWindowResize"
+import Page from "../../components/Page"
+import { CanvasWrapper } from "../../components/CanvasWrapper"
+import { Canvas } from "../../components/Canvas"
+import { CanvasZoomControl } from "../../components/CanvasZoomControl"
+import { CanvasLayerControl } from "../../components/CanvasLayerControl"
+import { Navigator } from "../../components/Navigator"
+import { Sidepanel2, Sidepanel2Controller } from "../../components/Sidepanel2"
+import { OverlayProvider, useOverlay } from "../../components/Overlay"
+import { ChevronDownIcon, ChevronUpIcon, InformationCircleIcon, PaperAirplaneIcon } from "@heroicons/react/24/solid"
+import Icon from "../../components/Icon"
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react"
 import { SouthpoleMap } from "./SouthPole"
+import { description } from "./SouthPoleData"
 
 interface SouthPoleViewProps {
-  map: SouthpoleMap;
-  inner?: ReactNode;
+  map: SouthpoleMap
+  inner?: ReactNode
 }
 
 export default function SouthPoleView({ map, inner }: SouthPoleViewProps) {
-  const [window] = useWindowResize();
-  const leftSidepanel = useRef<Sidepanel2Controller>(null);
+  const [window] = useWindowResize()
+  const leftSidepanel = useRef<Sidepanel2Controller>(null)
   const [section, setSection] = useState<{ [key: string]: boolean }>({
     "1": false,
     "2": false,
     "3": false,
     "4": false,
     "5": false,
-  });
+  })
 
   const setActiveSection = () => {
     // map.controller().setVisibility(1, !section[1]);
-    setSection((section) => ({ ...section, [1]: !section[1] }));
-  };
+    setSection((section) => ({ ...section, [1]: !section[1] }))
+  }
 
   return (
     <Page>
@@ -43,7 +44,7 @@ export default function SouthPoleView({ map, inner }: SouthPoleViewProps) {
           visible
           width={400}
           closable
-          label="Information"
+          label="The Project"
           ref={leftSidepanel}
           full={window.width < 600}
           className="bg-gray-950 border-r-white/20 border-r text-gray-400"
@@ -51,28 +52,23 @@ export default function SouthPoleView({ map, inner }: SouthPoleViewProps) {
             className: "scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-900",
           }}
         >
-          <div className="text-white text-center   leading-relaxed mb-4 border-b  border-gray-600 cursor-pointer border flex flex-col items-center justify-center p-1">
+          <div className="text-white text-center border-b border-gray-600 leading-relaxed mb-4 cursor-pointer flex flex-col items-center justify-center pb-4">
             <p className="uppercase text-2xl p-2">
-            The Race
+              The Race
             </p>
             <p>
-            to the
+              to the
             </p>
             <p className="uppercase text-2xl p-2">
-            South Pole
+              South Pole
             </p>
-            
           </div>
-          <Information />
-
           <p className="py-4">
-          The South Pole was first reached by Norwegian explorer Roald Amundsen and his team on December 14, 1911, during the Fram Expedition. 
-          Amundsen’s team arrived ahead of British explorer Robert Falcon Scott, who reached the pole on January 17, 1912. 
-          Tragically, Scott and his entire team perished on the return journey due to harsh conditions and a lack of supplies.
+            {description}
           </p>
           {
             map.expeditions.map((expedition) => (
-              <InformationSection key={expedition.id} title={expedition.name} open={section[expedition.id]} onClick={() => setSection((section) => ({ ...section, [expedition.id]: !section[expedition.id] }))}>
+              <InformationSection key={expedition.id} title={<InformationTitle title={expedition.name} year={expedition.year} />} open={section[expedition.id]} onClick={() => setSection((section) => ({ ...section, [expedition.id]: !section[expedition.id] }))}>
                 {expedition.description}
               </InformationSection>
             ))
@@ -87,7 +83,14 @@ export default function SouthPoleView({ map, inner }: SouthPoleViewProps) {
         </CanvasWrapper>
       </OverlayProvider>
     </Page>
-  );
+  )
+}
+
+const InformationTitle = ({ title, year }: { title: string, year: string }) => { 
+  return <div className="flex w-full justify-between">
+    <h1>{title}</h1>
+    <p className=" text-gray-500 mr-2">{year}</p>
+  </div>
 }
 
 const InformationSection = ({
@@ -96,10 +99,10 @@ const InformationSection = ({
   open = false,
   onClick,
 }: {
-  title: string;
-  children?: any;
-  open?: boolean;
-  onClick?: () => void;
+  title: ReactNode
+  children?: any
+  open?: boolean
+  onClick?: () => void
 }) => {
   return (
     <>
@@ -111,11 +114,11 @@ const InformationSection = ({
         <DisclosurePanel className="text-gray-300/80">{children}</DisclosurePanel>
       </Disclosure>
     </>
-  );
-};
+  )
+}
 
 const Information = () => {
-  const overlay = useOverlay();
+  const overlay = useOverlay()
 
   const showOverlay = () => {
     overlay?.showOverlay({
@@ -126,19 +129,19 @@ const Information = () => {
       children: (
         <>
           <h1 className="pb-8 text-2xl px-4 ">
-            
+
           </h1>
           <div className="flex flex-col lg:flex-row items-start justify-start">
 
           </div>
         </>
       ),
-    });
-  };
+    })
+  }
 
   return (
     <div className="justify-start w-full flex">
       <Icon onClick={() => showOverlay()} primary={InformationCircleIcon} label="Projekt" />
     </div>
-  );
-};
+  )
+}
