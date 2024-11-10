8c0391e71f816ace46a65c340436671afc9fbd69
John Doe <johndoe@example.com>
2024-11-10 13:30:44 +0100

    southpole quote added

--- src/content/southpole/SouthPole.tsx
+++ src/content/southpole/SouthPole.tsx
@@ -8,16 +8,36 @@ import { Expedition, expeditions } from './SouthPoleData'
 import SouthPoleView from './SouthPoleView'
 
 export type SouthpoleMap = Artwork & {
-  expeditions: Expedition[];
-  controller?: SouthPoleMapController;
+  expeditions: Expedition[]
+  controller?: SouthPoleMapController
 }
 
 export interface SouthPoleMapController {
-  setVisibility?: (id: string, visible: boolean) => void 
-  onClick?: (id: string) => void 
+  setVisibility?: (id: string, visible: boolean) => void
+  onClick?: (id: string) => void
 }
 
-const controller : SouthPoleMapController = { 
+const controller: SouthPoleMapController = {
+
+}
+
+const SouthPoleQuote = () => {
+
+  return <>
+    <div className="text-slate-700 tracking-widestleading-loose text-3xl flex justify-center items-center absolute w-full h-full bg-white/70">
+      <div>
+        <p className='max-w-96 font-serif leading-relaxed'>
+          “The eternal silence of the great white desert. Cloudy columns of snow drift advancing from the south, pale yellow wraiths, heralding the coming storm, blotting out one by one the sharp-cut lines of the land.”
+        </p>
+        <p className='text-lg mt-8 text-slate-500'>
+          Robert Falcon Scott, <br/> Scott's Last Expedition: The Journals
+        </p>
+      </div>
+
+    </div>
+    <ImageLoader src='https://stsvengrav.blob.core.windows.net/stsvengrav/southpole/southpole-base.jpg' />
+
+  </>
 
 }
 
@@ -28,10 +48,16 @@ export const southPole: SouthpoleMap = {
     'The seven most important voyages of discovery to the South Pole.',
   year: 2024,
   size: { width: 3400, height: 2600 },
-  defaultIndex: 2,
+  defaultIndex: 3,
   expeditions: expeditions,
   controller: controller,
   layer: [
+    {
+      id: 'quote',
+      inner: <SouthPoleQuote />,
+      name: 'Robert Falcon Scott',
+      description: 'Robert Falcon Scotts journals, Scotts Last Expedition, document his harrowing journey to the South Pole, filled with vivid descriptions of the relentless Antarctic landscape and extreme conditions that he and his team faced.',
+    },
     {
       id: 'map',
       name: 'South Pole',
@@ -61,3 +87,4 @@ export const southPolePage: PageDescription = {
 }
 
 export default southPolePage
+
