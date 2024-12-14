73a235532b4ef108df5f0a9891304c53470c96e0
John Doe <johndoe@example.com>
2024-12-15 00:12:46 +0100

    containe size hook

--- src/hooks/useContainerSize.tsx
+++ src/hooks/useContainerSize.tsx
@@ -6,22 +6,9 @@ interface ContainerSize {
 }
 
 /**
- * Custom hook that returns the size of the window and the size of a container element.
+ * Custom hook that returns the size of a container element.
  *
  * @param ref - A mutable reference object pointing to the container element.
- * @returns A tuple containing the window size and the container size.
- *
- * The window size is an object with `width` and `height` properties.
- * The container size is an object with `width` and `height` properties or `undefined` if the container is not available.
- *
- * @example
- * const ref = useRef(null);
- * const [windowSize, containerSize] = useContainerSize(ref);
- *
- * useEffect(() => {
- *   console.log('Window size:', windowSize);
- *   console.log('Container size:', containerSize);
- * }, [windowSize, containerSize]);
  */
 export const useContainerSize = (ref: MutableRefObject<any>): ContainerSize => {
   const [size, setSize] = useState<Size | undefined>()
