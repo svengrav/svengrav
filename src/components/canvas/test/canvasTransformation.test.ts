99d2f873f1fa402a7714a4be50dab1db1084c965
John Doe <johndoe@example.com>
2024-12-27 11:18:23 +0100

    canvas test fixed

--- src/components/canvas/test/canvasTransformation.test.ts
+++ src/components/canvas/test/canvasTransformation.test.ts
@@ -1,31 +1,15 @@
 
 
 import { canvasTransformation } from "@components/canvas/canvasTransformation"
-import { Artwork } from "@components/artwork/Artwork"
 import { Size } from "@core/geometry"
 
 describe('transformation', () => {
   it('initialize should calculate the correct canvas context', () => {
-    const layer = {
-      id: '',
-      name: '',
-      inner: undefined
-    }
-    const artwork: Artwork = {
-      size: { width: 100, height: 200 },
-      layer: [layer, layer, layer],
-      id: '',
-      name: '',
-      description: '',
-      year: 0,
-      defaultIndex: 1
-    }
+
+    const artworkProperties = { index: 1, noOfLayer: 3, size: { width: 100, height: 200 }}
     const canvasSize: Size = { width: 100, height: 100 }
 
-    const result = canvasTransformation.initialize(artwork, canvasSize)
-
-
-    expect(result.artwork).toEqual(artwork)
+    const result = canvasTransformation.initialize(artworkProperties, canvasSize)
     expect(result.size).toEqual(canvasSize)
     expect(result.transformation.overflow).toBe('fit')
     expect(result.transformation.size).toEqual({ width: 50, height: 100 })
@@ -39,24 +23,10 @@ describe('transformation', () => {
   }),
 
   it('initialize should calculate the correct canvas layers', () => {
-    const layer = {
-      id: '',
-      name: '',
-      inner: undefined
-    }
-    const artwork: Artwork = {
-      size: { width: 100, height: 200 },
-      layer: [layer],
-      id: '',
-      name: '',
-      description: '',
-      year: 0,
-      defaultIndex: 1
-    }
+    const artworkProperties = { index: 1, noOfLayer: 1, size: { width: 100, height: 200 }}
     const canvasSize: Size = { width: 100, height: 100 }
 
-    const result = canvasTransformation.initialize(artwork, canvasSize)
-
+    const result = canvasTransformation.initialize(artworkProperties, canvasSize)
     expect(result.transformation.layer.index).toBe(1)
     expect(result.transformation.layer.progress).toBe(100)
     expect(result.transformation.layer.layers.length).toBe(1)
